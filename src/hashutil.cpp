d0bd2c4899e25482e1b1489b6d1afc52674af5e6
Aaron <pinatamostgrim@gmail.com>
2022-11-14 23:23:19 -0400

    Perform transformations and return MD5 digest
    - Remove BufferA-BufferD as I didn't use them
    - Modify the sine table generation so I can use numbers listed in the spec
    - Adjust the way the digest is returned to the method caller
    
    Note: This implementation currently DOES NOT work and there remain bugs that throw the digest off.

--- src/hashutil.cpp
+++ src/hashutil.cpp
@@ -20,10 +20,11 @@ global_variable int MAX_ARGS = 1;
 
 struct message
 {
-    uint8 *MessagePtr;
-    uint32 MessageLengthBits;
-    uint32 PaddingLengthBits;
-    uint32 TotalLengthBits;
+    uint8 *MessagePtr = 0;
+    uint32 MessageLengthBits = 0;
+    uint32 PaddingLengthBits = 0;
+    uint32 TotalLengthBits = 0;
+    char Digest[33] = {};
 };
 
 
@@ -61,7 +62,6 @@ MD5GetPaddingLengthBits(uint32 messageLengthBits)
 
     return paddingLength;
 }
-
 
 internal uint32
 MD5AuxF(uint32 x, uint32 y, uint32 z)
@@ -144,7 +144,8 @@ MD5TransformII(uint32 A, uint32 B, uint32 C, uint32 D, uint32 X, int S, uint32 T
     return B + result;
 }
 
-internal char*
+
+internal void
 GetMD5Hash(message *message)
 {
     // Apply 1 padding to message
@@ -170,21 +171,124 @@ GetMD5Hash(message *message)
     uint32 C = 0x98badcfe;
     uint32 D = 0x10325476;
 
-    uint32 *BufferA = &A;
-    uint32 *BufferB = &B;
-    uint32 *BufferC = &C;
-    uint32 *BufferD = &D;
-
     // Generate sin table T
-    uint32 T[64] = {};
-    for (int i = 0; i < 64; ++i)
+    uint32 T[65] = {};
+    for (int i = 0; i < 65; ++i)
     {
-        double s = sin(i + 1);
+        double s = sin(i);
         s = s < 0 ? s * -1 : s;
         T[i] = uint32(s * 4294967296);
     }
 
-    return (char *)"";
+    uint32 X[16] = {};
+
+    for (uint32 i = 0;
+         (i * 16) < (message->TotalLengthBits / 8);
+         i++)
+    {
+        for (int j = 0; j < 16; ++j)
+        {
+            X[j] = *(message->MessagePtr + (i * 16) + j);
+        }
+
+        // Snapshot initial state of buffers
+        uint32 AA = A;
+        uint32 BB = B;
+        uint32 CC = C;
+        uint32 DD = D;
+
+        // Perform transformations
+        // Round 1
+        A = MD5TransformFF(A, B, C, D, X[0], 7, T[1]);
+        D = MD5TransformFF(D, A, B, C, X[1], 12, T[2]);
+        C = MD5TransformFF(C, D, A, B, X[2], 17, T[3]);
+        B = MD5TransformFF(B, C, D, A, X[3], 22, T[4]);
+
+        A = MD5TransformFF(A, B, C, D, X[4], 7, T[5]);
+        D = MD5TransformFF(D, A, B, C, X[5], 12, T[6]);
+        C = MD5TransformFF(C, D, A, B, X[6], 17, T[7]);
+        B = MD5TransformFF(B, C, D, A, X[7], 22, T[8]);
+
+        A = MD5TransformFF(A, B, C, D, X[8], 7, T[9]);
+        D = MD5TransformFF(D, A, B, C, X[9], 12, T[10]);
+        C = MD5TransformFF(C, D, A, B, X[10], 17, T[11]);
+        B = MD5TransformFF(B, C, D, A, X[11], 22, T[12]);
+
+        A = MD5TransformFF(A, B, C, D, X[12], 7, T[13]);
+        D = MD5TransformFF(D, A, B, C, X[13], 12, T[14]);
+        C = MD5TransformFF(C, D, A, B, X[14], 17, T[15]);
+        B = MD5TransformFF(B, C, D, A, X[15], 22, T[16]);
+
+        // Round 2
+        A = MD5TransformGG(A, B, C, D, X[1], 5, T[17]);
+        D = MD5TransformGG(D, A, B, C, X[6], 9, T[18]);
+        C = MD5TransformGG(C, D, A, B, X[11], 14, T[19]);
+        B = MD5TransformGG(B, C, D, A, X[0], 20, T[20]);
+
+        A = MD5TransformGG(A, B, C, D, X[5], 5, T[21]);
+        D = MD5TransformGG(D, A, B, C, X[10],9, T[22]);
+        C = MD5TransformGG(C, D, A, B, X[15], 14, T[23]);
+        B = MD5TransformGG(B, C, D, A, X[4], 20, T[24]);
+
+        A = MD5TransformGG(A, B, C, D, X[9], 5, T[25]);
+        D = MD5TransformGG(D, A, B, C, X[14],9, T[26]);
+        C = MD5TransformGG(C, D, A, B, X[3], 14, T[27]);
+        B = MD5TransformGG(B, C, D, A, X[8], 20, T[28]);
+
+        A = MD5TransformGG(A, B, C, D, X[13], 5, T[29]);
+        D = MD5TransformGG(D, A, B, C, X[2], 9, T[30]);
+        C = MD5TransformGG(C, D, A, B, X[7], 14, T[31]);
+        B = MD5TransformGG(B, C, D, A, X[12], 20, T[32]);
+
+        // Round 3
+        A = MD5TransformHH(A, B, C, D, X[5], 4, T[33]);
+        D = MD5TransformHH(D, A, B, C, X[8], 11, T[34]);
+        C = MD5TransformHH(C, D, A, B, X[11], 16, T[35]);
+        B = MD5TransformHH(B, C, D, A, X[14], 23, T[36]);
+
+        A = MD5TransformHH(A, B, C, D, X[1], 4, T[37]);
+        D = MD5TransformHH(D, A, B, C, X[4], 11, T[38]);
+        C = MD5TransformHH(C, D, A, B, X[7], 16, T[39]);
+        B = MD5TransformHH(B, C, D, A, X[10], 23, T[40]);
+
+        A = MD5TransformHH(A, B, C, D, X[13], 4, T[41]);
+        D = MD5TransformHH(D, A, B, C, X[0], 11, T[42]);
+        C = MD5TransformHH(C, D, A, B, X[3], 16, T[43]);
+        B = MD5TransformHH(B, C, D, A, X[6], 23, T[44]);
+
+        A = MD5TransformHH(A, B, C, D, X[9], 4, T[45]);
+        D = MD5TransformHH(D, A, B, C, X[12], 11, T[46]);
+        C = MD5TransformHH(C, D, A, B, X[15], 16, T[47]);
+        B = MD5TransformHH(B, C, D, A, X[2], 23, T[48]);
+
+        // Round 4
+        A = MD5TransformII(A, B, C, D, X[0], 6, T[49]);
+        D = MD5TransformII(D, A, B, C, X[7], 10, T[50]);
+        C = MD5TransformII(C, D, A, B, X[14], 15, T[51]);
+        B = MD5TransformII(B, C, D, A, X[5], 21, T[52]);
+
+        A = MD5TransformII(A, B, C, D, X[12], 6, T[53]);
+        D = MD5TransformII(D, A, B, C, X[3], 10, T[54]);
+        C = MD5TransformII(C, D, A, B, X[10], 15, T[55]);
+        B = MD5TransformII(B, C, D, A, X[1], 21, T[56]);
+
+        A = MD5TransformII(A, B, C, D, X[8], 6, T[57]);
+        D = MD5TransformII(D, A, B, C, X[15], 10, T[58]);
+        C = MD5TransformII(C, D, A, B, X[6], 15, T[59]);
+        B = MD5TransformII(B, C, D, A, X[13], 21, T[60]);
+
+        A = MD5TransformII(A, B, C, D, X[4], 6, T[61]);
+        D = MD5TransformII(D, A, B, C, X[11], 10, T[62]);
+        C = MD5TransformII(C, D, A, B, X[2], 15, T[63]);
+        B = MD5TransformII(B, C, D, A, X[9], 21, T[64]);
+
+        A = A + AA;
+        B = B + BB;
+        C = C + CC;
+        D = D + DD;
+    }
+
+    sprintf_s(message->Digest, 33, "%x%x%x%x", A, B, C, D);
 }
 
 
@@ -226,8 +330,8 @@ int main(int argc, char const *argv[])
     // Copy the message into the allocated memory
     MemoryCopy((uint8 *)messagePtr, (uint8 *)message.MessagePtr, (message.TotalLengthBits / 8));
 
-    char *hash = GetMD5Hash(&message);
-    printf("hash - %s\n", hash);
+    GetMD5Hash(&message);
+    printf("%s\n", message.Digest);
 
     return returnCode;
 }
