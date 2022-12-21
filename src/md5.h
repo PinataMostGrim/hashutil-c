5e250422675afe397af829d997b414282a471207
Aaron <pinatamostgrim@gmail.com>
2022-12-20 20:40:26 -0400

    Hard-code MD5's sin table values
    Replace run-time sine table calculation with pre-calculated values as 'MD5UpdateHash()' is now called for every 64byte block in the data. Sine table values originally generated with the following logic:
    
    uint32 T[65] = {};
    for (int i = 0; i < 65; ++i)
    {
        double s = sin(i);
        s = s < 0 ? s * -1 : s;
        T[i] = uint32(s * 4294967296);
    }

--- src/md5.h
+++ src/md5.h
@@ -1,6 +1,5 @@
 #if !defined(MD5_H)
 
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -133,13 +132,6 @@ MD5UpdateHash(md5_context *context, unsigned char *ptr, uint32 byteLength)
     uint32 block[16] = {};
 
     // Generate sin table T
-    uint32 T[65] = {};
-    for (int i = 0; i < 65; ++i)
-    {
-        double s = sin(i);
-        s = s < 0 ? s * -1 : s;
-        T[i] = uint32(s * 4294967296);
-    }
 
     // Note (Aaron): Iterate over 64 byte blocks of the data
     // 'i' represents the byte position in the data
@@ -168,88 +160,88 @@ MD5UpdateHash(md5_context *context, unsigned char *ptr, uint32 byteLength)
 
         // Perform transformations
         // Round 1
-        A = MD5TransformFF(A, B, C, D, block[0], 7, T[1]);
-        D = MD5TransformFF(D, A, B, C, block[1], 12, T[2]);
-        C = MD5TransformFF(C, D, A, B, block[2], 17, T[3]);
-        B = MD5TransformFF(B, C, D, A, block[3], 22, T[4]);
+        A = MD5TransformFF(A, B, C, D, block[0], 7, 0xd76aa478);
+        D = MD5TransformFF(D, A, B, C, block[1], 12, 0xe8c7b756);
+        C = MD5TransformFF(C, D, A, B, block[2], 17, 0x242070db);
+        B = MD5TransformFF(B, C, D, A, block[3], 22, 0xc1bdceee);
 
-        A = MD5TransformFF(A, B, C, D, block[4], 7, T[5]);
-        D = MD5TransformFF(D, A, B, C, block[5], 12, T[6]);
-        C = MD5TransformFF(C, D, A, B, block[6], 17, T[7]);
-        B = MD5TransformFF(B, C, D, A, block[7], 22, T[8]);
+        A = MD5TransformFF(A, B, C, D, block[4], 7, 0xf57c0faf);
+        D = MD5TransformFF(D, A, B, C, block[5], 12, 0x4787c62a);
+        C = MD5TransformFF(C, D, A, B, block[6], 17, 0xa8304613);
+        B = MD5TransformFF(B, C, D, A, block[7], 22, 0xfd469501);
 
-        A = MD5TransformFF(A, B, C, D, block[8], 7, T[9]);
-        D = MD5TransformFF(D, A, B, C, block[9], 12, T[10]);
-        C = MD5TransformFF(C, D, A, B, block[10], 17, T[11]);
-        B = MD5TransformFF(B, C, D, A, block[11], 22, T[12]);
+        A = MD5TransformFF(A, B, C, D, block[8], 7, 0x698098d8);
+        D = MD5TransformFF(D, A, B, C, block[9], 12, 0x8b44f7af);
+        C = MD5TransformFF(C, D, A, B, block[10], 17, 0xffff5bb1);
+        B = MD5TransformFF(B, C, D, A, block[11], 22, 0x895cd7be);
 
-        A = MD5TransformFF(A, B, C, D, block[12], 7, T[13]);
-        D = MD5TransformFF(D, A, B, C, block[13], 12, T[14]);
-        C = MD5TransformFF(C, D, A, B, block[14], 17, T[15]);
-        B = MD5TransformFF(B, C, D, A, block[15], 22, T[16]);
+        A = MD5TransformFF(A, B, C, D, block[12], 7, 0x6b901122);
+        D = MD5TransformFF(D, A, B, C, block[13], 12, 0xfd987193);
+        C = MD5TransformFF(C, D, A, B, block[14], 17, 0xa679438e);
+        B = MD5TransformFF(B, C, D, A, block[15], 22, 0x49b40821);
 
         // Round 2
-        A = MD5TransformGG(A, B, C, D, block[1], 5, T[17]);
-        D = MD5TransformGG(D, A, B, C, block[6], 9, T[18]);
-        C = MD5TransformGG(C, D, A, B, block[11], 14, T[19]);
-        B = MD5TransformGG(B, C, D, A, block[0], 20, T[20]);
+        A = MD5TransformGG(A, B, C, D, block[1], 5, 0xf61e2562);
+        D = MD5TransformGG(D, A, B, C, block[6], 9, 0xc040b340);
+        C = MD5TransformGG(C, D, A, B, block[11], 14, 0x265e5a51);
+        B = MD5TransformGG(B, C, D, A, block[0], 20, 0xe9b6c7aa);
 
-        A = MD5TransformGG(A, B, C, D, block[5], 5, T[21]);
-        D = MD5TransformGG(D, A, B, C, block[10],9, T[22]);
-        C = MD5TransformGG(C, D, A, B, block[15], 14, T[23]);
-        B = MD5TransformGG(B, C, D, A, block[4], 20, T[24]);
+        A = MD5TransformGG(A, B, C, D, block[5], 5, 0xd62f105d);
+        D = MD5TransformGG(D, A, B, C, block[10],9, 0x2441453);
+        C = MD5TransformGG(C, D, A, B, block[15], 14, 0xd8a1e681);
+        B = MD5TransformGG(B, C, D, A, block[4], 20, 0xe7d3fbc8);
 
-        A = MD5TransformGG(A, B, C, D, block[9], 5, T[25]);
-        D = MD5TransformGG(D, A, B, C, block[14],9, T[26]);
-        C = MD5TransformGG(C, D, A, B, block[3], 14, T[27]);
-        B = MD5TransformGG(B, C, D, A, block[8], 20, T[28]);
+        A = MD5TransformGG(A, B, C, D, block[9], 5, 0x21e1cde6);
+        D = MD5TransformGG(D, A, B, C, block[14],9, 0xc33707d6);
+        C = MD5TransformGG(C, D, A, B, block[3], 14, 0xf4d50d87);
+        B = MD5TransformGG(B, C, D, A, block[8], 20, 0x455a14ed);
 
-        A = MD5TransformGG(A, B, C, D, block[13], 5, T[29]);
-        D = MD5TransformGG(D, A, B, C, block[2], 9, T[30]);
-        C = MD5TransformGG(C, D, A, B, block[7], 14, T[31]);
-        B = MD5TransformGG(B, C, D, A, block[12], 20, T[32]);
+        A = MD5TransformGG(A, B, C, D, block[13], 5, 0xa9e3e905);
+        D = MD5TransformGG(D, A, B, C, block[2], 9, 0xfcefa3f8);
+        C = MD5TransformGG(C, D, A, B, block[7], 14, 0x676f02d9);
+        B = MD5TransformGG(B, C, D, A, block[12], 20, 0x8d2a4c8a);
 
         // Round 3
-        A = MD5TransformHH(A, B, C, D, block[5], 4, T[33]);
-        D = MD5TransformHH(D, A, B, C, block[8], 11, T[34]);
-        C = MD5TransformHH(C, D, A, B, block[11], 16, T[35]);
-        B = MD5TransformHH(B, C, D, A, block[14], 23, T[36]);
+        A = MD5TransformHH(A, B, C, D, block[5], 4, 0xfffa3942);
+        D = MD5TransformHH(D, A, B, C, block[8], 11, 0x8771f681);
+        C = MD5TransformHH(C, D, A, B, block[11], 16, 0x6d9d6122);
+        B = MD5TransformHH(B, C, D, A, block[14], 23, 0xfde5380c);
 
-        A = MD5TransformHH(A, B, C, D, block[1], 4, T[37]);
-        D = MD5TransformHH(D, A, B, C, block[4], 11, T[38]);
-        C = MD5TransformHH(C, D, A, B, block[7], 16, T[39]);
-        B = MD5TransformHH(B, C, D, A, block[10], 23, T[40]);
+        A = MD5TransformHH(A, B, C, D, block[1], 4, 0xa4beea44);
+        D = MD5TransformHH(D, A, B, C, block[4], 11, 0x4bdecfa9);
+        C = MD5TransformHH(C, D, A, B, block[7], 16, 0xf6bb4b60);
+        B = MD5TransformHH(B, C, D, A, block[10], 23, 0xbebfbc70);
 
-        A = MD5TransformHH(A, B, C, D, block[13], 4, T[41]);
-        D = MD5TransformHH(D, A, B, C, block[0], 11, T[42]);
-        C = MD5TransformHH(C, D, A, B, block[3], 16, T[43]);
-        B = MD5TransformHH(B, C, D, A, block[6], 23, T[44]);
+        A = MD5TransformHH(A, B, C, D, block[13], 4, 0x289b7ec6);
+        D = MD5TransformHH(D, A, B, C, block[0], 11, 0xeaa127fa);
+        C = MD5TransformHH(C, D, A, B, block[3], 16, 0xd4ef3085);
+        B = MD5TransformHH(B, C, D, A, block[6], 23, 0x4881d05);
 
-        A = MD5TransformHH(A, B, C, D, block[9], 4, T[45]);
-        D = MD5TransformHH(D, A, B, C, block[12], 11, T[46]);
-        C = MD5TransformHH(C, D, A, B, block[15], 16, T[47]);
-        B = MD5TransformHH(B, C, D, A, block[2], 23, T[48]);
+        A = MD5TransformHH(A, B, C, D, block[9], 4, 0xd9d4d039);
+        D = MD5TransformHH(D, A, B, C, block[12], 11, 0xe6db99e5);
+        C = MD5TransformHH(C, D, A, B, block[15], 16, 0x1fa27cf8);
+        B = MD5TransformHH(B, C, D, A, block[2], 23, 0xc4ac5665);
 
         // Round 4
-        A = MD5TransformII(A, B, C, D, block[0], 6, T[49]);
-        D = MD5TransformII(D, A, B, C, block[7], 10, T[50]);
-        C = MD5TransformII(C, D, A, B, block[14], 15, T[51]);
-        B = MD5TransformII(B, C, D, A, block[5], 21, T[52]);
+        A = MD5TransformII(A, B, C, D, block[0], 6, 0xf4292244);
+        D = MD5TransformII(D, A, B, C, block[7], 10, 0x432aff97);
+        C = MD5TransformII(C, D, A, B, block[14], 15, 0xab9423a7);
+        B = MD5TransformII(B, C, D, A, block[5], 21, 0xfc93a039);
 
-        A = MD5TransformII(A, B, C, D, block[12], 6, T[53]);
-        D = MD5TransformII(D, A, B, C, block[3], 10, T[54]);
-        C = MD5TransformII(C, D, A, B, block[10], 15, T[55]);
-        B = MD5TransformII(B, C, D, A, block[1], 21, T[56]);
+        A = MD5TransformII(A, B, C, D, block[12], 6, 0x655b59c3);
+        D = MD5TransformII(D, A, B, C, block[3], 10, 0x8f0ccc92);
+        C = MD5TransformII(C, D, A, B, block[10], 15, 0xffeff47d);
+        B = MD5TransformII(B, C, D, A, block[1], 21, 0x85845dd1);
 
-        A = MD5TransformII(A, B, C, D, block[8], 6, T[57]);
-        D = MD5TransformII(D, A, B, C, block[15], 10, T[58]);
-        C = MD5TransformII(C, D, A, B, block[6], 15, T[59]);
-        B = MD5TransformII(B, C, D, A, block[13], 21, T[60]);
+        A = MD5TransformII(A, B, C, D, block[8], 6, 0x6fa87e4f);
+        D = MD5TransformII(D, A, B, C, block[15], 10, 0xfe2ce6e0);
+        C = MD5TransformII(C, D, A, B, block[6], 15, 0xa3014314);
+        B = MD5TransformII(B, C, D, A, block[13], 21, 0x4e0811a1);
 
-        A = MD5TransformII(A, B, C, D, block[4], 6, T[61]);
-        D = MD5TransformII(D, A, B, C, block[11], 10, T[62]);
-        C = MD5TransformII(C, D, A, B, block[2], 15, T[63]);
-        B = MD5TransformII(B, C, D, A, block[9], 21, T[64]);
+        A = MD5TransformII(A, B, C, D, block[4], 6, 0xf7537e82);
+        D = MD5TransformII(D, A, B, C, block[11], 10, 0xbd3af235);
+        C = MD5TransformII(C, D, A, B, block[2], 15, 0x2ad7d2bb);
+        B = MD5TransformII(B, C, D, A, block[9], 21, 0xeb86d391);
 
         context->State[0] += A;
         context->State[1] += B;
