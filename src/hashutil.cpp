a4115a51d5cee86afe41d9592c7daad5b3e4422c
Aaron <pinatamostgrim@gmail.com>
2022-12-25 01:47:25 -0400

    Add command line argument to MD5 hash a file rather than a message

--- src/hashutil.cpp
+++ src/hashutil.cpp
@@ -1,16 +1,52 @@
+#include <string.h>
 
 #include "hashutil.h"
 #include "md5.h"
 
-global_variable int MAX_ARGS = 1;
+global_variable int MAX_ARGS = 2;
 
 
 int main(int argc, char const *argv[])
 {
+    // Process command line arguments
+    bool processOptionalArgs = true;
+    bool fileFlag = false;
+    bool messageConsumed = false;
+    char *messagePtr = (char *)argv[0];
+
+    for (int i = 1; i < argc; ++i)
+    {
+        if(strcmp(argv[i], "--") == 0)
+        {
+            processOptionalArgs = false;
+            continue;
+        }
+
+        if (processOptionalArgs
+         && ((strncmp(argv[i], "-f", 2) == 0) || (strncmp(argv[i], "--file", 6) == 0)))
+        {
+            fileFlag = true;
+            continue;
+        }
+
+        if(!messageConsumed)
+        {
+            messagePtr = (char *)argv[i];
+            messageConsumed = true;
+        }
+    }
+
+    // Error out on invalid arguments
     int argCount = argc - 1;
-    if (argc == 1)
+    if (argCount == 0)
     {
         printf("Error: Incorrect number of command line arguments supplied; expected %i but received %i\n", MAX_ARGS, argCount);
+        return EXIT_FAILURE;
+    }
+
+    if(!messageConsumed)
+    {
+        printf("Message argument missing");
         return EXIT_FAILURE;
     }
 
@@ -19,10 +55,19 @@ int main(int argc, char const *argv[])
         printf("Warning: Too many command line arguments supplied; expected %i but received %i\n", MAX_ARGS, argCount);
     }
 
-    char *messagePtr = (char *)argv[1];
 
+    md5_context result = {};
+    if(fileFlag)
+    {
+        printf("Calculating MD5 hash for file \"%s\":\n", messagePtr);
+        result = MD5HashFile(messagePtr);
+    }
+    else
+    {
+        printf("Calculating MD5 hash for string \"%s\":\n", messagePtr);
+        result = MD5HashString(messagePtr);
+    }
 
-    md5_context result = MD5HashString(messagePtr);
     printf("%s\n", result.DigestStr);
 
     return EXIT_SUCCESS;
