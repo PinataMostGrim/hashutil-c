b64f0b1ef8aff4b0a342c53feaf5b178845a36c1
Aaron <pinatamostgrim@gmail.com>
2022-12-25 01:48:12 -0400

    Add temporary test assertions for different messages

--- src/hashutil.cpp
+++ src/hashutil.cpp
@@ -70,5 +70,55 @@ int main(int argc, char const *argv[])
 
     printf("%s\n", result.DigestStr);
 
+
+#if HASHUTIL_SLOW
+    // Note (Aaron): Debug code only, remove after testing is completed
+    if(strcmp((char *)messagePtr, "") == 0)
+    {
+        char target[33] = "d41d8cd98f00b204e9800998ecf8427e";
+        Assert(strcmp(result.DigestStr, target) == 0);
+    }
+
+    if(strcmp((char *)messagePtr, "a") == 0)
+    {
+        char target[33] = "0cc175b9c0f1b6a831c399e269772661";
+        Assert(strcmp(result.DigestStr, target) == 0);
+    }
+
+    if(strcmp((char *)messagePtr, "The quick brown fox jumps over the lazy dog") == 0)
+    {
+        char target[33] = "9e107d9d372bb6826bd81d3542a419d6";
+        Assert(strcmp(result.DigestStr, target) == 0);
+    }
+
+    // Note (Aaron): Test message that is greater than 64 bytes but has a remainder less than 56 bytes long
+    if(strcmp((char *)messagePtr, "The quick brown fox jumps over the lazy dog over and over and over and over and over again") == 0)
+    {
+        char target[33] = "3ee09c8a8bb31c3e275cd5143ea0f79a";
+        Assert(strcmp(result.DigestStr, target) == 0);
+    }
+
+    // Note (Aaron): Test message one byte less than the 56 bytes boundary
+    if(strcmp((char *)messagePtr, "The quick brown fox jumps over the lazy dog over and ov") == 0)
+    {
+        char target[33] = "ed1b637d9fc34800ecaf4e50095f382d";
+        Assert(strcmp(result.DigestStr, target) == 0);
+    }
+
+    // Note (Aaron): Test message with remainder greater than 56 bytes long
+    if(strcmp((char *)messagePtr, "The quick brown fox jumps over the lazy dog over and over and over and over and over and over and over and over and over again") == 0)
+    {
+        char target[33] = "e6cc782eea06cd67bf26125785ea805e";
+        Assert(strcmp(result.DigestStr, target) == 0);
+    }
+
+    if(fileFlag && strcmp(messagePtr, "test.txt") == 0)
+    {
+        char target[33] = "05831eb88a34bfe953de0afc2b43f46d";
+        Assert(strcmp(result.DigestStr, target) == 0);
+    }
+
+#endif
+
     return EXIT_SUCCESS;
 }
