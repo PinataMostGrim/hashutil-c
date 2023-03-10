17f6cd6b6b7edf9b85d94562dae7a914e20a91ab
Aaron <pinatamostgrim@proton.me>
2023-03-10 15:54:41 -0400

    Add build system for 'test-hashutil' to project

---
+++ build-test-hashutil.sh
@@ -0,0 +1,32 @@
+# Build script for hashutil-c.
+
+# Save the script's folder
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# Set the DEBUG environment variable to 0 if
+# it isn't already defined
+if [ -z $DEBUG ]
+then
+    DEBUG=0
+fi
+
+if [ $DEBUG = "1" ]
+then
+    # Making debug build
+    CompilerFlags="-g -DHASHUTIL_SLOW=1"
+else
+    # Making release build
+    CompilerFlags="-DHASHUTIL_SLOW=0"
+fi
+
+BuildFolder="bin"
+
+# Create build folder if it doesn't exist
+mkdir -p "$SCRIPT_DIR/$BuildFolder"
+
+# Change to the build folder (and redirect stdout to /dev/null and the redirect stderr to stdout)
+pushd $BuildFolder > /dev/null 2>&1
+
+# Compile hashutil
+clang $CompilerFlags "$SCRIPT_DIR/src/test-hashutil.cpp" -o "test-hashutil"
+popd > /dev/null 2>&1
