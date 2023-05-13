0370e51365dff6c24686ce16056eb3f42c2fec20
Aaron <pinatamostgrim@proton.me>
2023-05-13 19:49:10 -0300

    Enable static analysis when building 'hashutil' and test driver

--- build-hashutil.bat
+++ build-hashutil.bat
@@ -5,10 +5,10 @@
 :: NOTE: Set %DEBUG% to 1 for debug build
 IF [%DEBUG%] == [1] (
     :: Making debug build
-    set CompilerFlags=-nologo -Od -Gm- -MT -GR- -EHa- -Oi -W4 -FC -wd4996 -wd4201 -DHASHUTIL_SLOW=1 -Zi -DEBUG:FULL
+    set CompilerFlags=-nologo -Od -Gm- -MT -GR- -EHa- -Oi -W4 -FC -wd4996 -wd4201 -DHASHUTIL_SLOW=1 -Zi -DEBUG:FULL -analyze
 ) ELSE (
     :: Making release build
-    set CompilerFlags=-nologo -Od -Gm- -MT -GR- -EHa- -Oi -W4 -FC -wd4996 -wd4201 -DHASHUTIL_SLOW=0
+    set CompilerFlags=-nologo -Od -Gm- -MT -GR- -EHa- -Oi -W4 -FC -wd4996 -wd4201 -DHASHUTIL_SLOW=0 -analyze
 )
 
 set BuildFolder=bin
