eadeeea389c0532ad18b516ecf80c1736b5a707a
Aaron <pinatamostgrim@proton.me>
2023-05-15 21:45:01 -0300

    Add sha2 algorithm support to 'hashutil.c'
    - Adds command line arguments for sha2 algorithms
    - Updates the list argument with more supported algorithms
    - Reworks argument parsing to be more easily extendable
    - Adjust the command line output for readability and simplicity

--- src/hashutil.c
+++ src/hashutil.c
@@ -1,14 +1,97 @@
+/* TODO (Aaron):
+    - Add compare hash functionality ('-c' or '--compare' sets COMPARE_FLAG bool)
+    - Support case insensitive algorithm strings
+    - Add a method to each header for retrieving supported algorithms?
+
+    - Create library for parsing command line arguments
+    - Consider compiling hashutil with C++ to take advantage of some features (declare arra of const strings)
+    - Enforce C99 in build batch files
+*/
+
 #include <string.h>
+#include <assert.h>
 
 #define HASHUTIL_MD5_IMPLEMENTATION
 #include "md5.h"
 #define HASHUTIL_SHA1_IMPLEMENTATION
 #include "sha1.h"
+#define HASHUTIL_SHA2_IMPLEMENTATION
+#include "sha2.h"
+
+
+#define ArrayCount(Array) (sizeof(Array) / sizeof((Array)[0]))
+
+
+static char *HashAlgorithmMnemonics[] =
+{
+    "unknown",
+    "md5",
+    "sha1",
+    "sha224",
+    "sha256",
+    "sha384",
+    "sha512",
+    "sha512-224",
+    "sha512-256",
+};
+
+
+typedef enum hash_algorithm
+{
+    hash_unknown,
+    hash_md5,
+    hash_sha1,
+    hash_sha224,
+    hash_sha256,
+    hash_sha384,
+    hash_sha512,
+    hash_sha512_224,
+    hash_sha512_256,
+
+    hash_algorithm_count,
+
+} hash_algorithm;
+
+
+typedef struct arguments
+{
+    bool usageFlag;
+    bool listFlag;
+    bool fileFlag;
+    bool algorithmConsumed;
+    bool messageConsumed;
+    char *algorithmPtr;
+    hash_algorithm algorithm;
+    char *messagePtr;
+} arguments;
+
+
+hash_algorithm GetHashAlgorithm(char *algorithmPtr)
+{
+    for (int i = 0; i < hash_algorithm_count; ++i)
+    {
+        if (strcmp(algorithmPtr, HashAlgorithmMnemonics[i]) == 0)
+        {
+            return (hash_algorithm)i;
+        }
+    }
+
+    return hash_unknown;
+}
+
+
+char *GetHashMenemonic(hash_algorithm algorithm)
+{
+    static_assert(ArrayCount(HashAlgorithmMnemonics) == hash_algorithm_count,
+              "'hash_algorithm' and 'HashAlgorithmMnemonics' do not share the sane number of elements\n");
+
+    return HashAlgorithmMnemonics[algorithm];
+}
 
 
 static void PrintUsage()
 {
-    printf("usage: hashutil [-f] algorithm message\n\n");
+    printf("usage: hashutil [-l -f] algorithm message\n\n");
     printf("Produces a message or file digest using various hashing algorithms.\n\n");
 
     printf("positional arguments:\n");
@@ -17,34 +100,49 @@ static void PrintUsage()
     printf("\n");
 
     printf("options:\n");
+    printf("-l, --list\t\tList all supported hashing algorithms.");
     printf("-f, --file\t\tHashes a file. Message is treated as a path.\n");
     printf("\n");
 }
 
 
-int main(int argc, char const *argv[])
+static void PrintSupportedAlgorithms()
 {
-    // Process command line arguments
+    printf("Algorithms supported:\n");
+    printf(" ");
+    // Note (Aaron): Initialize 'i' to 1 to skip "unknown"
+    for (int i = 1; i < ArrayCount(HashAlgorithmMnemonics); ++i)
+    {
+        printf(" %s", HashAlgorithmMnemonics[i]);
+    }
+
+    printf("\n");
+}
+
+
+void ParseArgs(int argc, char const *argv[], arguments *arguments)
+{
     bool processOptionalArgs = true;
-    bool usageFlag = false;
-    bool listFlag = false;
-    bool fileFlag = false;
     bool algorithmConsumed = false;
-    char *algorithmPtr = (char *)"";
     bool messageConsumed = false;
-    char *messagePtr = (char *)"";
+
+    arguments->usageFlag = false;
+    arguments->listFlag = false;
+    arguments->fileFlag = false;
+    arguments->algorithmPtr = (char *)"";
+    arguments->messagePtr = (char *)"";
 
     for (int i = 1; i < argc; ++i)
     {
         if ((strncmp(argv[i], "-h", 2) == 0) || (strncmp(argv[i], "--help", 6) == 0))
         {
-            usageFlag = true;
+            arguments->usageFlag = true;
             break;
         }
 
-        if ((strncmp(argv[i], "-l", 2) == 0) || (strncmp(argv[i], "--help", 6) == 0))
+        if ((strncmp(argv[i], "-l", 2) == 0) || (strncmp(argv[i], "--list", 6) == 0))
         {
-            listFlag = true;
+            arguments->listFlag = true;
             break;
         }
 
@@ -57,91 +155,199 @@ int main(int argc, char const *argv[])
         if (processOptionalArgs
             && ((strncmp(argv[i], "-f", 2) == 0) || (strncmp(argv[i], "--file", 6) == 0)))
         {
-            fileFlag = true;
+            arguments->fileFlag = true;
             continue;
         }
 
         if (!algorithmConsumed)
         {
-            algorithmPtr = (char *)argv[i];
+            // TODO (Aaron): What kind of sanitization do I need to do to this input?
+            arguments->algorithmPtr = (char *)argv[i];
             algorithmConsumed = true;
             continue;
         }
 
         if (!messageConsumed)
         {
-            messagePtr = (char *)argv[i];
+            // TODO (Aaron): What kind of sanitization do I need to do to this input?
+            arguments->messagePtr = (char *)argv[i];
             messageConsumed = true;
             continue;
         }
     }
+}
 
-    if (usageFlag)
+
+int main(int argc, char const *argv[])
+{
+    arguments arguments;
+    ParseArgs(argc, argv, &arguments);
+
+    if (arguments.usageFlag)
     {
         PrintUsage();
         return 0;
     }
 
-    if (listFlag)
+    if (arguments.listFlag)
     {
-        printf("Algorithms supported:\n");
-        printf("  md5 sha1\n\n");
+        PrintSupportedAlgorithms();
         return 0;
     }
 
     // Error out on missing or invalid arguments
-    if (!algorithmConsumed)
+    if (strlen(arguments.algorithmPtr) == 0)
     {
-        printf("'algorithm' argument missing\n\n");
+        printf("'algorithm' argument missing\n");
         PrintUsage();
         return 1;
     }
 
-    if(!messageConsumed)
+    if(strlen(arguments.messagePtr) == 0)
     {
-        printf("'message' argument missing\n\n");
+        printf("'message' argument missing\n");
         PrintUsage();
         return 1;
     }
 
-    // Switch on algorithm selected
-    if (strcmp(algorithmPtr, "md5") == 0)
+    // Control flow on the selected algorithm and hash
+    hash_algorithm algorithm = GetHashAlgorithm(arguments.algorithmPtr);
+    printf("%s %s\t: %s\n",
+        GetHashMenemonic(algorithm),
+        arguments.fileFlag ? "[file]" : "[string]",
+        arguments.messagePtr);
+
+    char *digest;
+    switch (algorithm)
     {
-        md5_context result;
-        if(fileFlag)
+        case hash_md5:
         {
-            printf("Calculating MD5 hash for file \"%s\":\n", messagePtr);
-            result = MD5HashFile(messagePtr);
+            md5_context context;
+            if(arguments.fileFlag)
+            {
+                context = MD5HashFile(arguments.messagePtr);
+            }
+            else
+            {
+                context = MD5HashString(arguments.messagePtr);
+            }
+
+            digest = context.DigestStr;
+            break;
         }
-        else
+        case hash_sha1:
         {
-            printf("Calculating MD5 hash for string \"%s\":\n", messagePtr);
-            result = MD5HashString(messagePtr);
+            sha1_context context;
+            if(arguments.fileFlag)
+            {
+                context = SHA1HashFile(arguments.messagePtr);
+            }
+            else
+            {
+                context = SHA1HashString(arguments.messagePtr);
+            }
+
+            digest = context.DigestStr;
+            break;
+        }
+        case hash_sha224:
+        {
+            sha2_256_context context;
+            // if(arguments.fileFlag)
+            // {
+            //     context = SHA2_HashFileSHA256_224(arguments.messagePtr);
+            // }
+            // else
+            {
+                context = SHA2_HashStringSHA256_224(arguments.messagePtr);
+            }
+
+            digest = context.DigestStr;
+            break;
+        }
+        case hash_sha256:
+        {
+            sha2_256_context context;
+            // if(arguments.fileFlag)
+            // {
+            //     context = MD5HashFile(arguments.messagePtr);
+            // }
+            // else
+            {
+                context = SHA2_HashStringSHA256_256(arguments.messagePtr);
+            }
+
+            digest = context.DigestStr;
+            break;
+        }
+        case hash_sha384:
+        {
+            sha2_512_context context;
+            // if(arguments.fileFlag)
+            // {
+            //     context = MD5HashFile(arguments.messagePtr);
+            // }
+            // else
+            {
+                context = SHA2_HashStringSHA512_384(arguments.messagePtr);
+            }
+
+            digest = context.DigestStr;
+            break;
+        }
+        case hash_sha512:
+        {
+            sha2_512_context context;
+            // if(arguments.fileFlag)
+            // {
+            //     context = MD5HashFile(arguments.messagePtr);
+            // }
+            // else
+            {
+                context = SHA2_HashStringSHA512_512(arguments.messagePtr);
+            }
+
+            digest = context.DigestStr;
+            break;
+        }
+        case hash_sha512_224:
+        {
+            sha2_512_context context;
+            // if(arguments.fileFlag)
+            // {
+            //     context = MD5HashFile(arguments.messagePtr);
+            // }
+            // else
+            {
+                context = SHA2_HashStringSHA512_224(arguments.messagePtr);
+            }
+
+            digest = context.DigestStr;
+            break;
+        }
+        case hash_sha512_256:
+        {
+            sha2_512_context context;
+            // if(arguments.fileFlag)
+            // {
+            //     context = MD5HashFile(arguments.messagePtr);
+            // }
+            // else
+            {
+                context = SHA2_HashStringSHA512_256(arguments.messagePtr);
+            }
+
+            digest = context.DigestStr;
+            break;
         }
 
-        printf("%s\n", result.DigestStr);
-    }
-    else if (strcmp(algorithmPtr, "sha1") == 0)
-    {
-        sha1_context result;
-        if(fileFlag)
+        default:
         {
-            printf("Calculating SHA1 hash for file \"%s\":\n", messagePtr);
-            result = SHA1HashFile(messagePtr);
+            printf("Unsupported algorithm selected\n");
+            return 1;
         }
-        else
-        {
-            printf("Calculating SHA1 hash for string \"%s\":\n", messagePtr);
-            result = SHA1HashString(messagePtr);
-        }
-
-        printf("%s\n", result.DigestStr);
-    }
-    else
-    {
-        printf("Unsupported algorithm selected\n");
-        return 1;
     }
 
+    printf("%s\n", digest);
     return 0;
 }
